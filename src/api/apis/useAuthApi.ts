f5b5d28809f2b0fef51cd583b26771bb5d4927c5
Asher Lien <birdym.tw@gmail.com>
2025-10-27 17:45:01 +0800

    auth

--- src/api/apis/useAuthApi.ts
+++ src/api/apis/useAuthApi.ts
@@ -10,4 +10,52 @@ export const useAuthApi = {
       data: payload,
     })
   },
+  register: (payload: any): Promise<any> => {
+    return useApiRequest.post({
+      url: '/tw/member/doLogin',
+      data: payload,
+    })
+  },
+  logout: (payload: any): Promise<any> => {
+    return useApiRequest.post({
+      url: '/tw/member/doLogin',
+      data: payload,
+    })
+  },
+  refreshToken: (payload: any): Promise<any> => {
+    return useApiRequest.post({
+      url: '/tw/member/doLogin',
+      data: payload,
+    })
+  },
+  checkValidToken: (payload: any): Promise<any> => {
+    return useApiRequest.post({
+      url: '/tw/member/doLogin',
+      data: payload,
+    })
+  },
+  sendOTPtoEmail: (payload: any): Promise<any> => {
+    return useApiRequest.post({
+      url: '/tw/member/doLogin',
+      data: payload,
+    })
+  },
+  bindOTPtoEmail: (payload: any): Promise<any> => {
+    return useApiRequest.post({
+      url: '/tw/member/doLogin',
+      data: payload,
+    })
+  },
+  forgetPaswordSendOTPtoEmail: (payload: any): Promise<any> => {
+    return useApiRequest.post({
+      url: '/tw/member/doLogin',
+      data: payload,
+    })
+  },
+  changePasswordWithOTP: (payload: any): Promise<any> => {
+    return useApiRequest.post({
+      url: '/tw/member/doLogin',
+      data: payload,
+    })
+  },
 }
