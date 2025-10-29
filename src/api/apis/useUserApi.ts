9225c7e66901496d3cd2d38d0098fc14a1e76c3f
Asher Lien <birdym.tw@gmail.com>
2025-10-29 10:36:55 +0800

    wip

--- src/api/apis/useUserApi.ts
+++ src/api/apis/useUserApi.ts
@@ -4,6 +4,6 @@ import { ShowCurrentResponse } from '@/types'
 
 export const useUserApi = {
   showCurrent: (): AxiosPromise<ShowCurrentResponse> => {
-    return useApiRequest.get({ url: '/tw/member/doLogin' })
+    return useApiRequest.get({ url: '/users/showMe' })
   },
 }
