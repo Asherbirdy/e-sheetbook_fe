fafa452bf6f9450b8b637513e4bcbec3cee20bdf
Asher Lien <birdym.tw@gmail.com>
2025-10-30 10:47:56 +0800

    更新

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,14 +1,21 @@
-import {
-  Box, Heading,
-} from '@chakra-ui/react'
+import { Box, Heading } from '@chakra-ui/react'
 import { DashboardLayout } from '@/layout'
+import { useAuthApi, useUserApi } from '@/api'
+import { useQuery } from '@tanstack/react-query'
 
 const DashboardMain = () => {
+
+  const { data: userInfo } = useQuery({
+    queryKey: ['user'],
+    queryFn: () => useUserApi.showCurrent(),
+  })
   return (
     <DashboardLayout>
       <Box>
         <Heading>Dashboard Home</Heading>
         <div>Dashboard Main Content</div>
+        {userInfo?.data && <div>{userInfo.data.msg}</div>}
+        {!userInfo?.data && <div>loading...</div>}
       </Box>
     </DashboardLayout>
   )
