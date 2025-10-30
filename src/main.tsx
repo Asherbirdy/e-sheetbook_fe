ce6cb344d5337e17513f97a0165b013e7b0b41b9
Asher Lien <birdym.tw@gmail.com>
2025-10-30 10:39:26 +0800

    done

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,8 @@ import ReactDOM from 'react-dom/client'
 import React from 'react'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { BrowserRouter } from 'react-router-dom'
-import { Provider } from './components/ui/provider'
+import { ChakraProvider, defaultSystem } from '@chakra-ui/react'
+import { ColorModeProvider } from './components/ui/color-mode'
 import { App } from './App'
 import { Toaster } from './components/ui/toaster'
 
@@ -10,13 +11,15 @@ const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <Provider>
-      <Toaster />
-      <QueryClientProvider client={queryClient}>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </QueryClientProvider>
-    </Provider>
+    <ChakraProvider value={defaultSystem}>
+      <ColorModeProvider>
+        <Toaster />
+        <QueryClientProvider client={queryClient}>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </QueryClientProvider>
+      </ColorModeProvider>
+    </ChakraProvider>
   </React.StrictMode>,
 )
