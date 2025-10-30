ecf1088973cf0abc763019f39ef68925b15daadf
Asherbirdy <122598799+Asherbirdy@users.noreply.github.com>
2025-10-30 11:29:44 +0800

    Feat/身份jwt驗證機制 (#3)
    * 更新
    
    * 更新
    
    * done
    
    * 更新

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,14 +1,23 @@
-import {
-  Box, Heading,
-} from '@chakra-ui/react'
+import { Box, Heading } from '@chakra-ui/react'
 import { DashboardLayout } from '@/layout'
+import { useUserApi } from '@/api'
+import { useQuery } from '@tanstack/react-query'
 
 const DashboardMain = () => {
+
+  const { data: userInfo } = useQuery({
+    queryKey: ['user'],
+    queryFn: () => useUserApi.showCurrent(),
+  })
+
+  console.log(userInfo)
   return (
     <DashboardLayout>
       <Box>
         <Heading>Dashboard Home</Heading>
         <div>Dashboard Main Content</div>
+        {userInfo?.data && <pre>{JSON.stringify(userInfo.data, null, 2)}</pre>}
+        {!userInfo?.data && <div>loading...</div>}
       </Box>
     </DashboardLayout>
   )
