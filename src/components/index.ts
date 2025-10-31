589281f84df211b2a9b3210e55e86c58879f0d4e
Asher Lien <birdym.tw@gmail.com>
2025-10-31 11:03:54 +0800

    wip

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,9 +2,6 @@
 export * from './page/login/LoginForm'
 
 // ** common
-export * from './common/Dashboard/Sidebar/SidebarContent'
-export * from './common/Dashboard/Sidebar/NavItem'
-export * from './common/Dashboard/Sidebar/LinkItems'
 export * from './common/Dashboard/DashboardHeader'
 export * from './common/Website/WebsiteHeader'
 export * from './common/ColorModeSwitcher'
