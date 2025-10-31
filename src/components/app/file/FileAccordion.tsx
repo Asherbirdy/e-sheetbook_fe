3787a6d3d02e3975bbdaa59563d109124e4b7e0d
Asher Lien <birdym.tw@gmail.com>
2025-10-31 17:08:21 +0800

    更新

--- src/components/app/file/FileAccordion.tsx
+++ src/components/app/file/FileAccordion.tsx
@@ -55,7 +55,13 @@ export const FileAccordion = () => {
           </Accordion.ItemTrigger>
           <Accordion.ItemContent>
             <Accordion.ItemBody py="2" px="3">
-              {file.sheets && file.sheets.length > 0 ? (
+              {!file.sheets || file.sheets.length === 0 && (
+                <Text fontSize="xs" color="gray.500" py="2">
+                  此文件尚無表格
+                </Text>
+              )}
+
+              {file.sheets && file.sheets.length > 0 && (
                 <VStack gap="2" alignItems="stretch">
                   {file.sheets.map((sheet) => (
                     <Box
@@ -95,12 +101,9 @@ export const FileAccordion = () => {
                     </Box>
                   ))}
                 </VStack>
-              ) : (
-                <Text fontSize="xs" color="gray.500" py="2">
-                  此文件尚無表格
-                </Text>
               )}
             </Accordion.ItemBody>
+
           </Accordion.ItemContent>
         </Accordion.Item>
       ))}
