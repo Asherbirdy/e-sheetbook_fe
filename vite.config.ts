48fa4b07fa1eb6fc06a8189b7fc543fd60ac23a0
Asher Lien <birdym.tw@gmail.com>
2025-10-31 10:28:01 +0800

    更新

--- vite.config.ts
+++ vite.config.ts
@@ -4,33 +4,39 @@ import viteTsconfigPaths from 'vite-tsconfig-paths'
 import AutoImport from 'unplugin-auto-import/vite'
 import Pages from 'vite-plugin-pages'
 import * as path from 'path'
+import { loadEnv } from 'vite'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: '/',
-  plugins: [
-    react({
+export default defineConfig((config) => {
+
+  const env = loadEnv(config.mode, process.cwd())
+
+  return{
+    base: '/',
+    plugins: [
+      react({
       // 啟用 Preact Signals 自動追蹤
-      babel: { plugins: [['module:@preact/signals-react-transform']] },
-    }),
-    AutoImport({
-      imports: ['react'],
-      dts: true,
-    }),
-    viteTsconfigPaths(),
-    Pages({
-      dirs: 'src/pages',
-      extensions: ['tsx'],
-      exclude: ['**/components/**', '**/index.ts'],
-    }),
-  ],
-  server: {
-    open: true, // Open the browser when the dev server starts
-    port: 3000,
-  },
-  // test: {
-  //   globals: true,
-  //   environment: 'jsdom',
-  // },
-  resolve: { alias: { '@': path.resolve(__dirname, 'src') } },
+        babel: { plugins: [['module:@preact/signals-react-transform']] },
+      }),
+      AutoImport({
+        imports: ['react'],
+        dts: true,
+      }),
+      viteTsconfigPaths(),
+      Pages({
+        dirs: 'src/pages',
+        extensions: ['tsx'],
+        exclude: ['**/components/**', '**/index.ts'],
+      }),
+    ],
+    server: {
+      open: true,
+      port: Number(env.VITE_PORT) || 3000,
+    },
+    // test: {
+    //   globals: true,
+    //   environment: 'jsdom',
+    // },
+    resolve: { alias: { '@': path.resolve(__dirname, 'src') } },
+  }
 })
