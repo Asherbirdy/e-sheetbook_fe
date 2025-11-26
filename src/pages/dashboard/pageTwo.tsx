b0668ed3fa190e1e55f47d75d43f439bca24d422
Asherbirdy <122598799+Asherbirdy@users.noreply.github.com>
2025-11-26 17:46:21 +0800

    fix/layout架構(#21)

--- src/pages/dashboard/pageTwo.tsx
+++ src/pages/dashboard/pageTwo.tsx
@@ -1,14 +1,14 @@
 import { Box, Heading } from '@chakra-ui/react'
-import { DashboardLayout } from '@/layout'
+
 
 const SecondPage = () => {
   return (
-    <DashboardLayout>
+    <>
       <Box>
         <Heading>Second Page</Heading>
         <div>Second Page Content</div>
       </Box>
-    </DashboardLayout>
+    </>
   )
 }
 
