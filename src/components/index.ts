6606c9d4ef805816a83442f5425313cd44565ef0
Asher Lien <birdym.tw@gmail.com>
2025-11-27 17:44:45 +0800

    asd

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,9 +1,8 @@
 // ** pages
-export * from './page/Dashboard/Login/LoginForm'
-export * from './page/Dashboard/Register/RegisterForm'
+export * from './LoginForm'
+export * from './app/RegisterForm'
 
 // ** common
-export * from './page/Website/WebsiteHeader'
 export * from './page/ColorModeSwitcher'
 
 // ** home
@@ -11,9 +10,4 @@ export { default as HomeHeader } from './home/HomeHeader'
 export { default as MeasurableSection } from './home/MeasurableSection'
 export { default as TeamCalendar } from './home/TeamCalendar'
 
-// ** app - file
-export * from './app/file/FileAccordion'
-export * from './app/file/FileAccordion/FileAddButton'
-export * from './app/file/FileAccordion/FileAddSheetButton'
-export * from './app/file/FileSheetGrid'
 
