1a793fb753ca480bbc4cc2b10453e32ece4049e8
Asher Lien <birdym.tw@gmail.com>
2025-11-27 15:10:16 +0800

    更新

--- src/stores/app/useAuthStore.ts
+++ src/stores/app/useAuthStore.ts
@@ -2,25 +2,25 @@ import { create } from 'zustand'
 import { useAuthApi } from '@/api/useAuthApi'
 
 interface AuthStore {
-  isAuthenticated: boolean
+  isLogin: boolean
   setIsAuthenticated: (value: boolean) => void
   checkLogin: () => Promise<void>
 }
 
 export const useAuthStore = create<AuthStore>((set) => ({
-  isAuthenticated: false,
+  isLogin: false,
 
   // 設定認證狀態
-  setIsAuthenticated: (value: boolean) => set({ isAuthenticated: value }),
+  setIsAuthenticated: (value: boolean) => set({ isLogin: value }),
 
   // 檢查登入狀態
   checkLogin: async () => {
     const response = await useAuthApi.checkLogin()
 
     if (response.data?.status === 'success') {
-      set({ isAuthenticated: true })
+      set({ isLogin: true })
       return
     }
-    set({ isAuthenticated: false })
+    set({ isLogin: false })
   },
 }))
