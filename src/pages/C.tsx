dd92a4e61f80f693ed70ab5179d2f8bb3ff03182
Asher Lien <birdym.tw@gmail.com>
2025-11-27 17:11:16 +0800

    更新

--- src/pages/C.tsx
+++ src/pages/C.tsx
@@ -1,7 +1,8 @@
 import { Outlet, useLocation } from 'react-router-dom'
 import { HomeHeader } from '@/components'
-import { Box } from '@chakra-ui/react'
-import { motion } from 'framer-motion'
+import { Box, Center, Spinner } from '@chakra-ui/react'
+import { AnimatePresence, motion } from 'framer-motion'
+import { Suspense } from 'react'
 
 const CRoute = () => {
   const location = useLocation()
@@ -10,17 +11,27 @@ const CRoute = () => {
     <Box minH="100vh" bg="gray.50">
       <Box maxW="1000px" mx="auto">
         <HomeHeader />
-        <motion.div
-          key={location.pathname}
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          exit={{ opacity: 0 }}
-          transition={{ duration: 0.5, ease: 'easeInOut' }}
-        >
-          <Box py={8} px={8}>
-            <Outlet />
-          </Box>
-        </motion.div>
+        <AnimatePresence mode="wait">
+          <motion.div
+            key={location.pathname}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.5, ease: 'easeInOut' }}
+          >
+            <Box py={8} px={8}>
+              <Suspense
+                fallback={
+                  <Center h="200px">
+                    <Spinner size="xl" />
+                  </Center>
+                }
+              >
+                <Outlet />
+              </Suspense>
+            </Box>
+          </motion.div>
+        </AnimatePresence>
       </Box>
     </Box>
   )
