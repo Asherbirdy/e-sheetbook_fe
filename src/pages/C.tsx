a895ec7ed02bb000280af5047233481a0862c4bf
Asher Lien <birdym.tw@gmail.com>
2025-11-27 16:03:37 +0800

    更新

--- src/pages/C.tsx
+++ src/pages/C.tsx
@@ -2,25 +2,28 @@ import { Outlet } from 'react-router-dom'
 import { motion, AnimatePresence } from 'framer-motion'
 import { useLocation } from 'react-router-dom'
 import { HomeHeader } from '@/components'
+import { Box } from '@chakra-ui/react'
 
 const CRoute = () => {
   const location = useLocation()
 
   return (
     <>
-      <HomeHeader />
-      <AnimatePresence mode="wait">
-        <motion.div
-          key={location.pathname}
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          exit={{ opacity: 0 }}
-          transition={{ duration: 0.3 }}
-          style={{ width: '100%' }}
-        >
-          <Outlet />
-        </motion.div>
-      </AnimatePresence>
+      <Box maxW="1000px" mx="auto" py={4}>
+        <HomeHeader />
+        <AnimatePresence mode="wait">
+          <motion.div
+            key={location.pathname}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.3 }}
+            style={{ width: '100%' }}
+          >
+            <Outlet />
+          </motion.div>
+        </AnimatePresence>
+      </Box>
     </>
   )
 }
