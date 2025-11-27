499235819a43bceff5e96518cd9128c7074c4281
Asher Lien <birdym.tw@gmail.com>
2025-11-27 16:10:37 +0800

    更新

--- src/pages/C.tsx
+++ src/pages/C.tsx
@@ -8,23 +8,28 @@ const CRoute = () => {
   const location = useLocation()
 
   return (
-    <>
-      <Box maxW="1000px" mx="auto" py={4}>
+    <Box minH="100vh" bg="gray.50">
+      <Box maxW="1000px" mx="auto">
         <HomeHeader />
-        <AnimatePresence mode="wait">
-          <motion.div
-            key={location.pathname}
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-            transition={{ duration: 0.3 }}
-            style={{ width: '100%' }}
-          >
-            <Outlet />
-          </motion.div>
-        </AnimatePresence>
+        <Box>
+          <AnimatePresence initial={false}>
+            <motion.div
+              key={location.pathname}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              transition={{
+                duration: 0.3,
+                ease: 'easeInOut',
+              }}
+              style={{ width: '100%' }}
+            >
+              <Outlet />
+            </motion.div>
+          </AnimatePresence>
+        </Box>
       </Box>
-    </>
+    </Box>
   )
 }
 
