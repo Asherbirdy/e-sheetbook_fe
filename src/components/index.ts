aa9d442f7b40d2b07927faba28c188561cce375c
Asherbirdy <122598799+Asherbirdy@users.noreply.github.com>
2025-11-27 17:53:42 +0800

    Refactor/整理不要的1127 (#31)
    * page
    
    * e鞥心
    
    * asd
    
    * 更新
    
    * done

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,21 +1,13 @@
 // ** pages
-export * from './page/Dashboard/Login/LoginForm'
-export * from './page/Dashboard/Register/RegisterForm'
+export * from './form/LoginForm'
+export * from './form/RegisterForm'
 
 // ** common
-export * from './page/Dashboard/DashboardHeader'
-export * from './page/Website/WebsiteHeader'
 export * from './page/ColorModeSwitcher'
-export * from './page/Dashboard/DashboardSidebar'
 
 // ** home
 export { default as HomeHeader } from './home/HomeHeader'
 export { default as MeasurableSection } from './home/MeasurableSection'
 export { default as TeamCalendar } from './home/TeamCalendar'
 
-// ** app - file
-export * from './app/file/FileAccordion'
-export * from './app/file/FileAccordion/FileAddButton'
-export * from './app/file/FileAccordion/FileAddSheetButton'
-export * from './app/file/FileSheetGrid'
 
